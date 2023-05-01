79d4026ed68b54ac7a10ee88e89629949d087bbf
Tim Hämisch <haemtim@gmail.com>
2023-05-01 16:50:27 +0200

    plot.py now working

--- messungen/plot.py
+++ messungen/plot.py
@@ -1,44 +1,3 @@
-"""
-import pyshark
-import matplotlib.pyplot as plt
-
-# Open the capture file and extract the packets
-capture = pyshark.FileCapture('/home/tim/Documents/Uni/Informatik/S4/netzePS/nvs23_ps/messungen/Node_Python/raw100.pcap')
-packets = [p for p in capture]
-
-# Group the packets by transmission
-transmissions = {}
-for packet in packets:
-    seq = int(packet.udp.stream)
-    if seq not in transmissions:
-        transmissions[seq] = []
-    transmissions[seq].append(packet)
-
-# Create a graph for each transmission
-for i, (seq, packets) in enumerate(transmissions.items()):
-    # Extract the sequence numbers and timestamps from the packets
-    seq_nums = [int(p.udp.seq) for p in packets]
-    timestamps = [float(p.sniff_timestamp) for p in packets]
-
-    # Extract the timestamp of the first packet
-    start_time = float(packets[0].sniff_timestamp)
-
-    relative_timestamps = [t - start_time for t in timestamps]
-
-    # Plot the sequence numbers against the timestamps
-    plt.plot(relative_timestamps, seq_nums, label=f'T{i}', linewidth=1.5)
-
-# Set the axis labels and legend
-plt.xlabel('Time (s)')
-plt.ylabel('Sequence number')
-plt.legend()
-
-# Save the plot as an image
-plt.savefig('/home/tim/Documents/Uni/Informatik/S4/netzePS/nvs23_ps/messungen/Node_Python/plot100.png')
-
-plt.show()
-"""
-
 import pyshark
 import matplotlib.pyplot as plt
 
@@ -52,15 +11,19 @@ for packet in packets:
     seq = packet.udp.stream
     if seq not in transmissions:
         transmissions[seq] = {'packets': [], 'seq_num': 0}
-    packet.udp.seq = transmissions[seq]['seq_num']
+    #packet.udp.seq = transmissions[seq]['seq_num']
     transmissions[seq]['seq_num'] += 1
     transmissions[seq]['packets'].append(packet)
+
+# Get the maximum packet number across all transmissions
+max_packet_num = max([len(data['packets']) for seq, data in transmissions.items()])
 
 # Create a graph for each transmission
 for i, (seq, data) in enumerate(transmissions.items()):
     packets = data['packets']
     # Extract the sequence numbers and timestamps from the packets
-    seq_nums = [int(p.udp.seq) for p in packets]
+    print(len(packets))
+    seq_nums = [i for i in range(len(packets))]
     timestamps = [float(p.sniff_timestamp) for p in packets]
 
     # Extract the timestamp of the first packet
@@ -75,6 +38,9 @@ for i, (seq, data) in enumerate(transmissions.items()):
 plt.xlabel('Time (s)')
 plt.ylabel('Sequence number')
 plt.legend()
+
+# Set the y-axis ticks to natural numbers
+plt.yticks(range(max_packet_num), range(max_packet_num))
 
 # Save the plot as an image
 plt.savefig('/home/tim/Documents/Uni/Informatik/S4/netzePS/nvs23_ps/messungen/Node_Python/plot100.png')
