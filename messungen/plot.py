e5f1812260b94f2908f01eabcbcf80a798128606
Julian <julianoberhofer2@gmail.com>
2023-05-02 22:22:18 +0200

    automated all plots

--- messungen/plot.py
+++ messungen/plot.py
@@ -1,46 +1,61 @@
 import pyshark
 import matplotlib.pyplot as plt
+import asyncio
 
-# Open the capture file and extract the packets
-capture = pyshark.FileCapture('/home/tim/Documents/Uni/Informatik/S4/netzePS/nvs23_ps/messungen/Node_Python/raw100.pcap')
-packets = [p for p in capture]
 
-# Group the packets by transmission and assign sequence numbers
-transmissions = {}
-for packet in packets:
-    seq = packet.udp.stream
-    if seq not in transmissions:
-        transmissions[seq] = {'packets': [], 'seq_num': 0}
-    transmissions[seq]['seq_num'] += 1
-    transmissions[seq]['packets'].append(packet)
+def main():
+    folders = ['Dart_Java', 'Dart_Python', 'Node_Java', 'Node_Python'] 
+    numbers = [100, 1400, 60000]
+    for folder in folders:
+        for number in numbers:
+            # Open the capture file and extract the packets
+            capture = pyshark.FileCapture(folder + '/raw' + str(number) + '.pcap')
+            packets = [p for p in capture]
 
-# Get the maximum packet number across all transmissions
-max_packet_num = max([len(data['packets']) for seq, data in transmissions.items()])
+            # Group the packets by transmission and assign sequence numbers
+            transmissions = {}
+            for packet in packets:
+                seq = packet.udp.stream
+                if seq not in transmissions:
+                    transmissions[seq] = {'packets': [], 'seq_num': 0}
+                transmissions[seq]['seq_num'] += 1
+                transmissions[seq]['packets'].append(packet)
 
-# Create a graph for each transmission
-for i, (seq, data) in enumerate(transmissions.items()):
-    packets = data['packets']
-    # Extract the sequence numbers and timestamps from the packets
-    seq_nums = [i for i in range(len(packets))]
-    timestamps = [float(p.sniff_timestamp) for p in packets]
+            # Get the maximum packet number across all transmissions
+            max_packet_num = max([len(data['packets']) for seq, data in transmissions.items()])
 
-    # Extract the timestamp of the first packet
-    start_time = float(packets[0].sniff_timestamp)
+            # Create a graph for each transmission
+            for i, (seq, data) in enumerate(transmissions.items()):
+                packets = data['packets']
+                # Extract the sequence numbers and timestamps from the packets
+                seq_nums = [i for i in range(len(packets))]
+                timestamps = [float(p.sniff_timestamp) for p in packets]
 
-    relative_timestamps = [t - start_time for t in timestamps]
+                # Extract the timestamp of the first packet
+                start_time = float(packets[0].sniff_timestamp)
 
-    # Plot the sequence numbers against the timestamps
-    plt.plot(relative_timestamps, seq_nums, label=f'T{i}', linewidth=1.5)
+                relative_timestamps = [(t - start_time) * 1000 for t in timestamps]
+                # print(relative_timestamps)
 
-# Set the axis labels and legend
-plt.xlabel('Time (s)')
-plt.ylabel('Sequence number')
-plt.legend()
+                # Plot the sequence numbers against the timestamps
+                plt.plot(relative_timestamps, seq_nums, label=f'T{i}', linewidth=1.5)
 
-# Set the y-axis ticks to natural numbers
-plt.yticks(range(max_packet_num), range(max_packet_num))
+            # Set the axis labels and legend
+            plt.xlabel('Time (ms)')
+            plt.ylabel('Sequence number')
+            plt.legend()
 
-# Save the plot as an image
-plt.savefig('/home/tim/Documents/Uni/Informatik/S4/netzePS/nvs23_ps/messungen/Node_Python/plot100.png')
+            # Set the y-axis ticks to natural numbers
+            if max_packet_num < 20:
+                plt.yticks(range(max_packet_num), range(max_packet_num))
+            else:
+                plt.yticks(range(0, max_packet_num, 2), range(0, max_packet_num, 2))
 
-plt.show()
+            # Save the plot as an image
+            plt.savefig(folder + '/plot' + str(number) + '.png')
+            plt.close()
+
+# plt.show()
+
+if __name__ == '__main__':
+    main()
