260411621fa5ade0ada01f4f0e3ef22c3d76c381
Julian <julianoberhofer2@gmail.com>
2023-05-04 00:08:05 +0200

    chance runner to subsystem for better controll

--- autoRun.py
+++ autoRun.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import subprocess
+import time
 
 # Create an argument parser
 parser = argparse.ArgumentParser(description='Process some command line arguments.')
@@ -9,6 +11,7 @@ parser.add_argument('--tx', type=str)
 parser.add_argument('--rx', type=str)
 parser.add_argument('--max', type=str, default="1500")
 parser.add_argument('--amount', type=int, default=10)
+parser.add_argument('--timeout', type=int, default=5)
 
 # Parse the arguments
 args = parser.parse_args()
@@ -18,6 +21,7 @@ tx = args.tx.lower()
 rx = args.rx.lower()
 max_pack = args.max
 amount = args.amount
+timeout = args.timeout
 
 # TX-Variables
 dart_path = "TX/Dart/tx/lib/tx.dart"
@@ -31,22 +35,35 @@ java_path = "RX/Java/rx_java/src/UDPReceiver"
 for i in range(amount):
     # Execute the RX script
     if rx == "python":
-        os.system("python " + python_path + " --max " + max_pack + " &")
+        rx_proc = subprocess.Popen(['python', python_path, '--max', max_pack])
     elif rx == "java":
         os.system("javac " + java_path + ".java")
-        os.system("java " + java_path + ".class --max " + max_pack + " &")
+        rx_proc = subprocess.Popen(['java', '-classpath', 'RX/Java/rx_java/src', 'UDPReceiver', '--max', max_pack])
     else:
         print("Invalid RX name entered")
-        break
+        exit()
+
     # Execute the TX script
     if tx == "dart":
-        os.system("dart run " + dart_path + " --max " + max_pack)
+        tx_proc = subprocess.Popen(['dart', 'run', dart_path, '--max', max_pack])
     elif tx == "node":
-        os.system("node " + node_path + " --max " + max_pack)
+        tx_proc = subprocess.Popen(['node', node_path, '--max', max_pack])
     else:
         print("Invalid TX name entered")
         break
-    print("Packet " + str(i) + " sent!")
+    # print("Packet " + str(i) + " sent!")
+    
+    # Wait for the TX process to complete before terminating the RX process
+    tx_proc.wait()
+    for i in range(timeout):
+        rx_exit_code = rx_proc.poll()
+        if rx_exit_code is not None:
+            # RX process has terminated
+            break
+        time.sleep(1)
+    # RX process is still running, terminate it
+    rx_proc.terminate()
+    rx_proc.wait()
+    
 
 print("Is Great Succes!")
-    
