f675fcb3ceaf6de10979ad4ee31f849ef49531c2
Julian <julianoberhofer2@gmail.com>
2023-05-04 00:18:30 +0200

    fix

--- mainMessung.py
+++ mainMessung.py
@@ -4,8 +4,8 @@ import subprocess
 
 messungs_folder = "messungen_test"
 
-tx_list = ["dart", "node"] # List of all senders
-rx_list = ["java", "python"] # List of all receivers
+tx_list = ["Dart", "Node"] # List of all senders
+rx_list = ["Java", "Python"] # List of all receivers
 pktlen_list = [100, 1400, 60_000] # List of all packet lengths
 
 
@@ -13,7 +13,7 @@ for tx in tx_list:
     for rx in rx_list:
         for pktlen in pktlen_list:
             foldername = f"{messungs_folder}/{rx}_{tx}"
-            filename = f"{pktlen}.pcap"
+            filename = f"raw{pktlen}.pcap"
             os.makedirs(foldername, exist_ok=True) # Create directory if it doesn't exist
             capture = pyshark.LiveCapture(interface='Adapter for loopback traffic capture', output_file=f"{foldername}/{filename}")
             # capture.sniff(timeout=10) # Capture network traffic for 10 seconds
