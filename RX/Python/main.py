97133c3da26d547fc0ed4ec71bf4a04d77c20c0f
Tim Hämisch <haemtim@gmail.com>
2023-05-16 16:01:50 +0200

    added --quiet mode

--- RX/Python/main.py
+++ RX/Python/main.py
@@ -10,6 +10,7 @@ if len(sys.argv) > 1:
         print('  --host <host>       Host to send to (default: 127.0.0.1)');
         print('  --port <port>       Port to send to (default: 12345)');
         print('  --max <size>        Maximum packet size (default: 1500)');
+        print('  --quiet             Do not print anything to the terminal');
         print('  --help              Show this help');
         sys.exit(0);
 
@@ -20,6 +21,7 @@ parser = argparse.ArgumentParser(description='Process some command line argument
 parser.add_argument('--host', type=str, default='127.0.0.1', help='Host to send to (default: 127.0.0.1)')
 parser.add_argument('--port', type=int, default=12345, help='Port to send to (default: 12345)')
 parser.add_argument('--max', type=int, default=1500, help='Maximum packet size (default: 1500)')
+parser.add_argument('--quiet', type=bool, default=False, help='Do not print anything to the terminal')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -28,6 +30,7 @@ args = parser.parse_args()
 HOST = args.host
 PORT = args.port
 max_pack = args.max
+quiet = args.quiet
 
 # Define the variables
 seq_num = 0
@@ -36,8 +39,9 @@ final_data = b''
 # Create a UDP socket and bind it to the host and port
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, PORT))
-print(f'Listening on {HOST}:{PORT}')
-print('---------------------------------')
+if not quiet:
+    print(f'Listening on {HOST}:{PORT}')
+    print('---------------------------------')
 
 # Receive the first packet
 data, addr = sock.recvfrom(max_pack)
@@ -46,7 +50,8 @@ max_seq_num = int.from_bytes(data[6:10], byteorder='big')
 file_name_length = len(data) - 10
 file_nameU = data[10:10+file_name_length].decode('utf-8')
 file_name = re.sub(r'.*/', '', file_nameU)
-print(f'Packet 0 (init): id={id}, maxSeqNum={max_seq_num}, fileName={file_name}')
+if not quiet:
+    print(f'Packet 0 (init): id={id}, maxSeqNum={max_seq_num}, fileName={file_name}')
 
 # Send response packet containing transmission ID and sequence number
 response_data = id.to_bytes(2, byteorder='big') + seq_num.to_bytes(4, byteorder='big')
@@ -59,7 +64,8 @@ while seq_num < max_seq_num:
     seq_num = int.from_bytes(data[2:6], byteorder='big')
     packet_data = (data[6:])
     final_data += packet_data
-    print(f'Packet {seq_num}: id={id}, data={packet_data}')
+    if not quiet:
+        print(f'Packet {seq_num}: id={id}, data={packet_data}')
 
     # Send response packet containing transmission ID and sequence number
     response_data = id.to_bytes(2, byteorder='big') + seq_num.to_bytes(4, byteorder='big')
@@ -70,25 +76,31 @@ data, addr = sock.recvfrom(max_pack)
 id = int.from_bytes(data[0:2], byteorder='big')
 seq_num = int.from_bytes(data[2:6], byteorder='big')
 md5 = data[6:22].hex()
-print(f'Packet {seq_num} (md5): id={id}, md5={md5}')
+if not quiet:
+    print(f'Packet {seq_num} (md5): id={id}, md5={md5}')
 
 # Send response packet containing transmission ID and sequence number
 response_data = id.to_bytes(2, byteorder='big') + seq_num.to_bytes(4, byteorder='big')
 sock.sendto(response_data, addr)
 
-print('---------------------------------')
+if not quiet:
+    print('---------------------------------')
 
 # Verify the MD5
 final_data_md5 = hashlib.md5(final_data).hexdigest()
 
 if md5 == final_data_md5:
-    print(u'\u2714''  MD5 matches')
+    if not quiet:
+        print(u'\u2714''  MD5 matches')
     with open(file_name, 'wb') as f:
         f.write(final_data)
-    print(u'\u2714'f'  File {file_name} written')
+    if not quiet:
+        print(u'\u2714'f'  File {file_name} written')
 else:
-    print(u'\u274C'' MD5 does not match: \n 'f' {md5} != {final_data_md5}')
+    if not quiet:
+        print(u'\u274C'' MD5 does not match: \n 'f' {md5} != {final_data_md5}')
 
 # Close the socket
 sock.close()
-print(u'\u2714''  Socket closed')
+if not quiet:
+    print(u'\u2714''  Socket closed')
