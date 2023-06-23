a04f34fe489757defac650eeafd0a3ee1312d131
Tim Hämisch <haemtim@gmail.com>
2023-06-23 20:06:23 +0200

    removed signal based timeout once again

--- RX/Python/main.py
+++ RX/Python/main.py
@@ -4,14 +4,14 @@ import hashlib
 import sys
 import argparse
 import io
-import signal
+#import signal
 import time
 
-class TimeoutException(Exception):
+""" class TimeoutException(Exception):
     pass
 
 def timeout_handler(signum, frame):
-    raise TimeoutException("Function timed out")
+    raise TimeoutException("Function timed out") """
 
 if len(sys.argv) > 1:
     if sys.argv[1] == '--help' or sys.argv[1] == '-h':
@@ -126,16 +126,16 @@ elif version == 3:
             packet_missing = False
             packet_was_missing = True
         # Timeout for receiving packet
-        signal.signal(signal.SIGALRM, timeout_handler)
+        """ signal.signal(signal.SIGALRM, timeout_handler)
         signal.alarm(1)
-        try:
-            data, addr = sock.recvfrom(max_pack)
-            id = int.from_bytes(data[0:2], byteorder='big')
-            seq_num = int.from_bytes(data[2:6], byteorder='big')
-        except TimeoutException:
+        try: """
+        data, addr = sock.recvfrom(max_pack)
+        id = int.from_bytes(data[0:2], byteorder='big')
+        seq_num = int.from_bytes(data[2:6], byteorder='big')
+        """ except TimeoutException:
             sendDupAckBySQN(seq_num-1)
             print(f'Packet {seq_num-1} is missing, sending duplicate ACK')
-            continue
+            continue """
         if id == transmID and seq_num >= window_start and seq_num <= window_end:
             # Test duplicate ACKs by skipping the 3rd packet once
             if seq_num == 3 and not skippedAlready:
