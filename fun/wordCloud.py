47d049d6f0857a259454ad717766b2cf21c68013
Tim Hämisch <haemtim@gmail.com>
2023-06-23 23:40:46 +0200

    workflow test

--- fun/wordCloud.py
+++ fun/wordCloud.py
@@ -4,15 +4,15 @@ import subprocess
 
 # Get all GitHub commit messages via command line
 # git log --pretty=format:%s > commitMessages.txt
-#subprocess.call(['git', 'log', '--pretty=format:%s', '>', 'commitmsgs.txt'])
+subprocess.call(['git', 'log', '--pretty=format:%s', '>', 'commitmsgs.txt'])
 with open('commitmsgs.txt', 'r') as f:
     textParts = f.read().split('\n')
 
 for textPart in textParts:
     if "merge" in textPart.lower():
         textParts.remove(textPart)
-    if "wordcloud" or "worldcloud" in textPart.lower():
-        textParts.remove(textPart)
+    #if "wordcloud" or "worldcloud" in textPart.lower():
+    #    textParts.remove(textPart)
 
 text = ' '.join(textParts)
 
